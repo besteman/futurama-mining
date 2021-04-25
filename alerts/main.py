826994289a389841bd9f274ea6b1e4ba83dc96f0
besteman <ellioy37@gmail.com>
2021-04-25 17:56:18 -0400

    check hashrate and send SMS

--- alerts/main.py
+++ alerts/main.py
@@ -7,53 +7,63 @@ from twilio.rest import Client
 
 load_dotenv()
 
-account_sid = os.getenv('account_sid')
-auth_token = os.getenv('auth_token')
+ACCOUNT_SID = os.getenv('account_sid')
+AUTH_TOKEN = os.getenv('auth_token')
 
 BASE_ETH_URL = 'https://api.nanopool.org/v1/eth/'
 ETH_MINER_ADDRESS = '0x5d78c71912ea88c23c602c8e0d5363d1e3cba4be'
 BASE_LIMIT = 950
+PHONE_NUMBERS = ['+19413570978', '+19896074589']
 
 
-def get_miner_reported_hashrate():
+def get_workers_reported_hashrate():
     # https://api.nanopool.org/v1/eth/hashrate/:address
     # response = requests.get(BASE_ETH_URL + ETH_MINER_ADDRESS)
-    response = requests.get(f'{BASE_ETH_URL}reportedhashrate/{ETH_MINER_ADDRESS}')
+    response = requests.get(f'{BASE_ETH_URL}reportedhashrates/{ETH_MINER_ADDRESS}')
     response = response.json()
 
     return response['data']
 
 
-def send_txt_msg():
-    # client = Client(account_sid, auth_token)
+def check_workers_hashrate(workers_hashrate):
+    offline_workers = []
 
-    # client.api.account.messages.create(
-    #     to="+19413570978",
-    #     from_="+12132925602",
-    #     body="Hello World!")
+    for worker in workers_hashrate:
+        if worker['hashrate'] == 0:
+            offline_workers.append(worker['worker'])
+
+    return offline_workers
+
+
+def send_txt_msg(offline_workers):
+    txt_body = f"""Panic! At the Hashrate! \n{', '.join(offline_workers)} rigs are reporting 0 hashrate"""
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    for phone_number in PHONE_NUMBERS:
+
+        client.api.account.messages.create(
+            to=phone_number,
+            from_="+12132925602",
+            body=txt_body)
 
 
 def main():
     print("Starting")
 
-    reported_hashrate = get_miner_reported_hashrate()
+    workers_hashrate = get_workers_reported_hashrate()
 
-    print(reported_hashrate)
+    print(workers_hashrate)
 
-    if reported_hashrate < BASE_LIMIT:
-        send_txt_msg()
+    offline_workers = check_workers_hashrate(workers_hashrate)
+
+    print(offline_workers)
+
+    if offline_workers:
+        send_txt_msg(offline_workers)
     else:
-        print(f'Reported hashrate is not below the {BASE_LIMIT}')
+        print(f'No Workers are not below the {BASE_LIMIT}')
 
-
-
-
-# client = Client(account_sid, auth_token)
-
-# client.api.account.messages.create(
-#     to="+19413570978",
-#     from_="+12132925602",
-#     body="Hello World!")
 
 if __name__ == "__main__":
     main()
