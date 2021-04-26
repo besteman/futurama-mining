2f5630842eab2e9f52fd44ae171aada3d0744968
besteman <ellioy37@gmail.com>
2021-04-25 20:36:39 -0400

    refactor to send email which will send text

--- alerts/main.py
+++ alerts/main.py
@@ -9,16 +9,16 @@ load_dotenv()
 
 ACCOUNT_SID = os.getenv('account_sid')
 AUTH_TOKEN = os.getenv('auth_token')
+GMAIL_ADDRESS = os.getenv('gmail_address')
+GMAIL_PW = os.getenv('gmail_pw')
 
 BASE_ETH_URL = 'https://api.nanopool.org/v1/eth/'
 ETH_MINER_ADDRESS = '0x5d78c71912ea88c23c602c8e0d5363d1e3cba4be'
 BASE_LIMIT = 950
-PHONE_NUMBERS = ['+19413570978', '+19896074589']
+EMAIL_ADDRESS = ['9413570978@vtext.com', '+19896074589']
 
 
 def get_workers_reported_hashrate():
-    # https://api.nanopool.org/v1/eth/hashrate/:address
-    # response = requests.get(BASE_ETH_URL + ETH_MINER_ADDRESS)
     response = requests.get(f'{BASE_ETH_URL}reportedhashrates/{ETH_MINER_ADDRESS}')
     response = response.json()
 
@@ -35,17 +35,23 @@ def check_workers_hashrate(workers_hashrate):
     return offline_workers
 
 
-def send_txt_msg(offline_workers):
-    txt_body = f"""Panic! At the Hashrate! \n{', '.join(offline_workers)} rigs are reporting 0 hashrate"""
+def send_email(offline_workers):
+    email_body = f"""Panic! At the Hashrate! \n{', '.join(offline_workers)} rigs are reporting 0 hashrate"""
 
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    try:
+        server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        server.ehlo()
+        server.starttls()
+        server.login(GMAIL_ADDRESS, GMAIL_PW)
 
-    for phone_number in PHONE_NUMBERS:
+        subject = 'Panic! At the Hashrate!'
+        body = f'{', '.join(offline_workers)} rigs are reporting 0 hashrate'
 
-        client.api.account.messages.create(
-            to=phone_number,
-            from_="+12132925602",
-            body=txt_body)
+        message = f'Subject: {subject}\n\n{body}'
+        server.sendmail(GMAIL_ADDRESS, receiver, email_body)
+        server.quit()
+    except Exception as e:
+        print(f'Something went wrong... {e}')
 
 
 def main():
@@ -60,7 +66,7 @@ def main():
     print(offline_workers)
 
     if offline_workers:
-        send_txt_msg(offline_workers)
+        send_email(offline_workers)
     else:
         print(f'No Workers are not below the {BASE_LIMIT}')
 
