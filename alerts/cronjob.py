7bd21a8be8df3a07503e79c928312b82435a63e9
besteman <ellioy37@gmail.com>
2021-05-02 08:42:34 -0400

    add cronjob process

---
+++ alerts/cronjob.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from main import main
+
+# Create an instance of scheduler and add function.
+scheduler = BlockingScheduler()
+scheduler.add_job(main, "interval", seconds=30)
+
+scheduler.start()
