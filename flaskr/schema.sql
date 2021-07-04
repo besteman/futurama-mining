3d7206ceb6a6ceef643d2a929dd88114937eb350
besteman <ellioy37@gmail.com>
2021-07-03 21:07:20 -0400

    sql

---
+++ flaskr/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+INSERT INTO user (username, password)
+VALUES( 'besteman' , 123);
