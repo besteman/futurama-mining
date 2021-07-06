9520164b8e7f4670f300335ee757d30e68ddff8d
besteman <ellioy37@gmail.com>
2021-07-06 10:05:30 -0400

    sql changes for testing

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS miner;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,5 +7,18 @@ CREATE TABLE user (
   password TEXT NOT NULL
 );
 
+CREATE TABLE miner (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name INTEGER NOT NULL,
+  enabled BOOLEAN NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO user (username, password)
 VALUES( 'besteman' , 123);
+
+INSERT INTO miner (name, enabled) 
+VALUES ( 'this', 1 );
+
+INSERT INTO miner (name, enabled) 
+VALUES ( 'that', 0 );
