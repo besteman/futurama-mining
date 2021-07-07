b7fb3ac7d0bcfb4a48dbcb04f197392b639353a4
besteman <ellioy37@gmail.com>
2021-07-07 14:35:26 -0400

    move alerts folder

--- flaskr/alerts/cronjob.py
+++ flaskr/alerts/cronjob.py
@@ -1,9 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from main import main
+from flaskr.alerts import alerts
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(main, "interval", seconds=3600)
+scheduler.add_job(alert, "interval", seconds=10)
 
 scheduler.start()
