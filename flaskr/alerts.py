4ed166535c9c9d4c6b4e7e081f226c38493735c2
besteman <ellioy37@gmail.com>
2021-07-10 13:03:02 -0400

    logging

--- flaskr/alerts.py
+++ flaskr/alerts.py
@@ -8,6 +8,8 @@ from twilio.rest import Client
 
 from flaskr.db import get_db
 
+import logging
+
 
 load_dotenv()
 
@@ -36,7 +38,7 @@ def get_enabled_miners_from_db():
     for miner in enabled_miners:
         enabled_miners.append(enabled_miners_from_db['name'])
 
-    print (enabled_miners)
+    logging.info(enabled_miners)
 
     return enabled_miners
 
