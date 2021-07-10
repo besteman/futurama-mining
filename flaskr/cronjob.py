b5ca8233ecff40e5590075d69d367e938547c343
besteman <ellioy37@gmail.com>
2021-07-10 13:09:05 -0400

    fix import

--- flaskr/cronjob.py
+++ flaskr/cronjob.py
@@ -4,6 +4,6 @@ from flaskr.alerts import main
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(alerts, "interval", seconds=10)
+scheduler.add_job(main, "interval", seconds=10)
 
 scheduler.start()
