0f46254c80754e806046837d2a1ceb26d336af1e
besteman <ellioy37@gmail.com>
2021-07-24 13:58:19 -0400

    create db and databases models

---
+++ flaskr/extensions.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    miner = db.relationship('Miner', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"""
+        id: {self.id}
+        username: {self.username}
+        created_date: {self.created_at}
+    """
+
+class Miner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    enabled = db.Column(db.Boolean,  nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    created_user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+
+    def __repr__(self):
+        return f"""
+        id: {self.id}
+        name: {self.name}
+        enabled: {self.enabled}
+        created_date: {self.created_at}
+        created_user_id: {self.created_user_id}
+    """
