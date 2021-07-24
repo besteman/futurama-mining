7fa0d32b52c31218feddd1fd1e5f4d911e72a4e3
besteman <ellioy37@gmail.com>
2021-07-24 16:55:11 -0400

    remove print statement

--- flaskr/config.py
+++ flaskr/config.py
@@ -6,6 +6,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'instance/flaskr.db')
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
